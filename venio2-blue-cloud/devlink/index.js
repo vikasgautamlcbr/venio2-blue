b3c0bd8eda116b10bfa5c69c35983d2ce915c453
vikasgautamlcbr <vikasgautamlcbr@gmail.com>
2026-01-13 13:51:37 +0530

    update

---
+++ venio2-blue-cloud/devlink/index.js
@@ -0,0 +1,12 @@
+"use client";
+
+console.warn(
+  "⚠️ Avoid importing components from 'index.js' for better performance. This practice is deprecated and may be removed in the future."
+);
+
+export * as _Builtin from "./_Builtin";
+export * from "./devlinkContext";
+export * from "./interactions";
+export * from "./utils";
+export * from "./devlink";
+export * from "./DevLinkProvider";
