7e5f7a94d5088887429b3e03f2b8b9f666b60804
vikasgautamlcbr <vikasgautamlcbr@gmail.com>
2026-01-13 13:52:54 +0530

    update

--- venio2-blue-cloud/astro.config.mjs
+++ venio2-blue-cloud/astro.config.mjs
@@ -4,13 +4,11 @@ import cloudflare from "@astrojs/cloudflare";
 
 import react from '@astrojs/react';
 
-import userConfig from './clouduser.astro.config.mjs';
-
-const webflowOverrides = {
+// https://astro.build/config
+export default defineConfig({
   base: "/app",
   output: "server",
   adapter: cloudflare({
-    imageService: "custom",
     platformProxy: {
       enabled: true
     }
@@ -18,35 +16,12 @@ const webflowOverrides = {
 
   integrations: [react()],
   vite: {
-    ...userConfig.vite,
     resolve: {
-      ...userConfig.vite?.resolve,
       // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
       // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
       alias: import.meta.env.PROD ? {
         "react-dom/server": "react-dom/server.edge",
       } : undefined,
     },
-  },
-
-  build: {
-    assetsPrefix: "https://f40aa93a-2275-4df5-baec-8a83742842f4.wf-app-prod.cosmic.webflow.services/app"
-  },
-
-  image: {
-    ...userConfig.image,
-    service: {
-      entrypoint: "./webflow-loader.ts",
-      config: {
-        deployUrl: "https://f40aa93a-2275-4df5-baec-8a83742842f4.wf-app-prod.cosmic.webflow.services",
-        mountPath: "/app"
-      }
-    },
-  }
-};
-
-// https://astro.build/config
-export default defineConfig({
-  ...userConfig,
-  ...webflowOverrides,
+}
 });
